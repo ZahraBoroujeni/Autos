0433fa36fd1a89f743437124aff25789d25c06bf
zargol <zahra.boroujeni@gmail.com>
2015-05-04 19:27:35 +0200

    start

---
+++ my_code/src/talker.cpp
@@ -0,0 +1,25 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Float64.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc,argv,"talker");
+    ros::NodeHandle n;
+    ros::Publisher chatter_pub = n.advertise<std_msgs::Float64 >("/drc_vehicle_xp900/gas_pedal/cmd",1000);
+    ros::Rate loop_rate(1);
+
+    int count=0;
+    while (ros::ok())
+    {
+        std_msgs::Float64  msg;
+        msg.data=1;
+        chatter_pub.publish(msg);
+        ROS_INFO("I heard: [%f]", msg);
+        ros::spinOnce();
+        loop_rate.sleep();
+        ++count;
+
+    }
+    return 0;
+}
---
+++ test_car/README.md
@@ -0,0 +1,13 @@
+# gazebo_ros_test_car
+* Author: Zahra Boroujeni <zahra.boroujeni@gmail.com>
+* Inception Date: 29 December 2014
+* Version: 1.0.0
+
+
+## Documentation and Tutorials
+
+## Develop and Contribute
+
+
+
+
